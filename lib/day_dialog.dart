182a620dbdd31d717abdc48e74078e189141f706
Sharad Chand <sharad@sharadchand.com>
2018-11-22 21:35:18 +0530

    Color today dialog blue

--- lib/day_dialog.dart
+++ lib/day_dialog.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:maas/converter.dart';
 import 'package:maas/data/translations.dart';
 
-const borderRadius = 15.0;
+final _radius = 15.0;
+final _circularRadius = Radius.circular(_radius);
 
 class DayDialog extends StatelessWidget {
   final BSDate date;
@@ -13,16 +14,19 @@ class DayDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      child: Column(
-        children: <Widget>[
-          _DayBadge(date: date),
-          Flexible(child: _Events()),
-        ],
-        mainAxisSize: MainAxisSize.min,
-      ),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(borderRadius),
+    final theme = Theme.of(context).copyWith(
+      dialogBackgroundColor: Colors.transparent,
+    );
+    return Theme(
+      data: theme,
+      child: Dialog(
+        child: Column(
+          children: <Widget>[
+            _DayBadge(date: date),
+            Flexible(child: _Events()),
+          ],
+          mainAxisSize: MainAxisSize.min,
+        ),
       ),
     );
   }
@@ -35,18 +39,31 @@ class _DayBadge extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isToday = date.isToday();
     final textTheme = Theme.of(context).textTheme;
-    final largeText = textTheme.display4.apply(color: Colors.grey[800]);
-    final mediumText = textTheme.display1.apply(fontWeightDelta: -1);
+    final largeText = textTheme.display4.apply(
+      color: isToday ? Colors.grey[50] : Colors.grey[800],
+    );
+    final mediumText = textTheme.display1.apply(
+      fontWeightDelta: -1,
+      color: isToday ? Colors.grey[200] : Colors.grey[800],
+    );
     final smallText = textTheme.title.apply(
       fontWeightDelta: -2,
-      color: Colors.grey[400],
+      color: isToday ? Colors.blue[200] : Colors.grey[400],
     );
 
     final greg = date.toGregorian();
 
     return Container(
+      width: double.infinity,
       padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+          color: isToday ? Colors.blue : Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: _circularRadius,
+            topRight: _circularRadius,
+          )),
       child: Column(
         children: <Widget>[
           Text(
@@ -68,7 +85,14 @@ class _Events extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: borderRadius),
+      padding: EdgeInsets.only(bottom: _radius),
+      decoration: BoxDecoration(
+        color: Colors.grey[50],
+        borderRadius: BorderRadius.only(
+          bottomLeft: _circularRadius,
+          bottomRight: _circularRadius,
+        ),
+      ),
       child: ListView.separated(
         itemCount: 5,
         shrinkWrap: true,
